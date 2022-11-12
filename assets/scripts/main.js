5d506c1ab1fd9ad900f7942c0a7e2ead75f8e1c8
JC Yan <ahhorsegan@gmail.com>
2022-11-12 11:47:46 -0800

    Completed Explore

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -54,6 +54,25 @@ function initializeServiceWorker() {
   // B5. TODO - In the event that the service worker registration fails, console
   //            log that it has failed.
   // STEPS B6 ONWARDS WILL BE IN /sw.js
+  if('serviceWorker' in navigator){
+    addEventListener('load', async (event) => {
+      try {
+        const registration = await navigator.serviceWorker.register("/sw.js", { 
+          scope: "/",
+        });
+
+        if(registration.installing){
+          console.log("Service worker installing");
+        } else if (registration.waiting) {
+          console.log("Service worker installed");
+        } else if (registration.active) {
+          console.log("Service worker active");
+        }
+      } catch (error) {
+        console.error(`Registration failed with ${error}`);
+      }
+    });
+  }
 }
 
 /**
